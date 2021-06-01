e183c1d97bef28eca05b3612196cb2cd8e88b5b1
Crossy-swift <crossworm2011@gmail.com>
2021-06-01 18:42:50 +0300

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Артем Репин on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
